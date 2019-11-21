c48451a518c082b43be0a10872d6665b7e43b84a
hyoenju <hyeonju@teamlabui-MacBook-Pro-2.local>
2019-11-21 20:57:21 +0900

    add image_search

--- image_search/urls.py
+++ image_search/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='image_search'),
+    path('image_upload', views.image_upload, name='image_upload'),
 ]
--- image_search/views.py
+++ image_search/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from patent_search_system import settings
+
+import os
+
+
+def handle_uploaded_file(f):
+    path_to_img = settings.MEDIA_ROOT
+    img_path = os.path.join(path_to_img, f.name)
+    with open(img_path, 'wb+') as destination:
+        destination.write(f.read())
 
 
 def index(request):
     return render(request, 'image_search/index_2.html')
+
+
+def image_upload(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['image'])
+    return HttpResponse("success")
