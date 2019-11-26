8a77d943547dc4a68d86398b8702d8d5e86ba869
hyoenju <hyeonju@teamlabui-MacBook-Pro-2.local>
2019-11-26 16:30:34 +0900

    modified html, css, js(image_search)

--- image_search/urls.py
+++ image_search/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='image_search'),
+    path('2', views.index_2, name='image_search_2'),
     path('image_upload', views.image_upload, name='image_upload'),
 ]
