d9095155d397f0759d83417e07129f335b03d0f0
JONGSKY <ljhljh0125@gmail.com>
2019-12-03 23:48:18 +0900

    text_search clustering

--- text_search/urls.py
+++ text_search/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='text_search'),
     path('wordcloud', views.wordcloud_search, name='wordcloud'),
     path('text_result', views.text_result, name='text_result'),
+    path('clustering_map', views.clustering_map, name='clustering_map'),
 ]
--- text_search/views.py
+++ text_search/views.py
@@ -9,9 +9,14 @@ import operator
 
 from datetime import datetime
 # import random
-
+from sklearn.manifold import TSNE
+# import pandas as pd
+import pickle
+import re
 
 model = Word2Vec.load('word2vec.model')
+patent_list, embedding_list = pickle.load(open("patent_field_best_model.embed", "rb"))
+
 
 def index(request):
     return render(request, 'text_search/index.html')
@@ -35,13 +40,44 @@ def text_result(request):
     time_1 = datetime.now()
     print(time_1)
     data_list = Patent.objects.filter(reduce(operator.and_, (Q(abstract__contains=k) for k in keyword_list))).order_by('-date')
-    # data_list = data_list[::-1][:9]
-    # data_list = data_list[9::-1]
+
+    patent_id_list = list(data_list.values_list('patent_id', flat=True))
+    time = datetime.now()
+    print(time-time_1)
+    # print(len(data_list))
     data_list = data_list[:10000]
-    # time = datetime.now()
-    # print(time-time_1)
+    time = datetime.now()
+    print(time-time_1)
     data_list = list(data_list.values())
     time = datetime.now()
     print(time-time_1)
-    # print(data_list)
-    return JsonResponse(data_list, safe=False)
+    result = {"data_list" : data_list,
+                "patent_id_list" : patent_id_list}
+    return JsonResponse(result, safe=False)
+
+import json
+
+def clustering_map(request):
+    patent_id_list = request.GET['patent_id'].split(',')
+
+    cluster_patent_id = []
+    cluster_embedding = []
+    for i in patent_id_list:
+        number = int(re.findall('\d+', i)[0])
+        index_ = patent_list.index(number)
+        cluster_patent_id.append(number)
+        cluster_embedding.append(embedding_list[index_])
+
+    print(datetime.now())
+    tsne = TSNE(learning_rate=100)
+    transformed = tsne.fit_transform(cluster_embedding)
+
+    # print(datetime.now())
+    # df_new = pd.DataFrame()
+    # df_new['x'] = transformed[:, 0]
+    # df_new['y'] = transformed[:, 1]
+    result = {"x_value" : transformed[:, 0],
+                "y_value" : transformed[:, 1]}
+    print(result)
+    result = json.dumps(str(result))
+    return JsonResponse(result, safe=False)
